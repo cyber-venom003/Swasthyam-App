823bd8dd6ff9422238702bd232f1d2c20af0be06
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-11-08 08:49:10 +0530

    Changes UI in BrainTumor

--- lib/routes/brainTumour.dart
+++ lib/routes/brainTumour.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
@@ -12,19 +13,18 @@ class TumourInput extends StatefulWidget {
 class _TumourInputState extends State<TumourInput> {
   int tumourRisk = 0;
   void sendRequest(filename) async {
-    var request = http.MultipartRequest('POST', Uri.parse("http://192.168.1.7:5000/predict"));
+    var request = http.MultipartRequest(
+        'POST', Uri.parse("http://192.168.1.7:5000/predict"));
     request.files.add(await http.MultipartFile.fromPath('image', filename));
-    request.fields.addAll({
-      "disease": "brainTumour"
-    });
-    try{
+    request.fields.addAll({"disease": "brainTumour"});
+    try {
       var res = await request.send();
       var body = await res.stream.bytesToString();
       setState(() {
         tumourRisk = jsonDecode(body)['prediction'];
       });
       print(tumourRisk);
-    } catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
@@ -40,77 +40,83 @@ class _TumourInputState extends State<TumourInput> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-            title: Text("MRI Test Results")
-        ),
+        appBar: AppBar(title: Text("MRI Test Results")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              FlatButton(
-                onPressed: () {
-                  uploadImage();
-                },
-                child: Container(
-                  width: 230,
-                  height: 45,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Upload MRI Image",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(26.0),
-                    color: const Color(0xff3f9bfc),
-                    border:
-                    Border.all(width: 1.0, color: const Color(0xff3f9bfc)),
-                  ),
-                ),
-              ),
-              ListTile(
-                  title: Text("Your Tumour Risk",
-                      style: TextStyle(
-                        fontSize: 17.5,
-                        fontWeight: FontWeight.w500,
-                      )
-                  ),
-                  subtitle: tumourRisk > 80 && tumourRisk <= 100 ? Text("High Risk",
-                      style: TextStyle(
-                          fontSize: 22.5,
+              // FlatButton(
+              //   onPressed: () {
+              //     uploadImage();
+              //   },
+              //   child: Container(
+              //     width: 230,
+              //     height: 45,
+              //     child: Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Text(
+              //         "Upload MRI Image",
+              //         style: TextStyle(
+              //             color: Colors.white,
+              //             fontSize: 22.5,
+              //             fontWeight: FontWeight.w500
+              //         ),
+              //         textAlign: TextAlign.center,
+              //       ),
+              //     ),
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(26.0),
+              //       color: const Color(0xff3f9bfc),
+              //       border:
+              //       Border.all(width: 1.0, color: const Color(0xff3f9bfc)),
+              //     ),
+              //   ),
+              // ),
+
+              Card(
+                child: ListTile(
+                    leading: FaIcon(FontAwesomeIcons.fileInvoice),
+                    title: Text("Your Tumour Risk",
+                        style: TextStyle(
+                          fontSize: 17.5,
                           fontWeight: FontWeight.w500,
-                          color: Colors.red
-                      )
-                  ) : tumourRisk > 40 && tumourRisk < 80 ? Text("Medium Risk",
-                      style: TextStyle(
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.yellow
-                      )
-                  ) : tumourRisk < 40 && tumourRisk > 20 ? Text("Low Risk",
-                      style: TextStyle(
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.green
-                      )
-                  ) : Text("Not Detected",
-                      style: TextStyle(
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey
-                      )
-                  )
+                        )),
+                    subtitle: tumourRisk > 80 && tumourRisk <= 100
+                        ? Text("High Risk ($tumourRisk %)",
+                            style: TextStyle(
+                                fontSize: 22.5,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.red))
+                        : tumourRisk > 40 && tumourRisk < 80
+                            ? Text("Medium Risk ($tumourRisk %)",
+                                style: TextStyle(
+                                    fontSize: 22.5,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.yellow))
+                            : tumourRisk < 40 && tumourRisk > 20
+                                ? Text("Low Risk ($tumourRisk %)",
+                                    style: TextStyle(
+                                        fontSize: 22.5,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.green))
+                                : Text("Not Detected",
+                                    style: TextStyle(
+                                        fontSize: 22.5,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.grey))),
               )
             ],
           ),
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          icon: FaIcon(FontAwesomeIcons.fileUpload),
+          onPressed: () {
+            uploadImage();
+          },
+          label: Text("Upload MRI Image"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
   }
