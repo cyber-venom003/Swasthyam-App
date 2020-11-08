8afc465b6a436250b12aa3e8f2a2da4e21524704
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-11-08 08:50:17 +0530

    Changes UI in PneumoniaInput

--- lib/routes/pneumoniaInput.dart
+++ lib/routes/pneumoniaInput.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
@@ -12,18 +13,17 @@ class PneumoniaInput extends StatefulWidget {
 class _PneumoniaInputState extends State<PneumoniaInput> {
   int pneumoniaRisk = 0;
   void sendRequest(filename) async {
-    var request = http.MultipartRequest('POST', Uri.parse("http://192.168.1.7:5000/predict"));
+    var request = http.MultipartRequest(
+        'POST', Uri.parse("http://192.168.1.7:5000/predict"));
     request.files.add(await http.MultipartFile.fromPath('image', filename));
-    request.fields.addAll({
-      "disease": "pneumonia"
-    });
-    try{
+    request.fields.addAll({"disease": "pneumonia"});
+    try {
       var res = await request.send();
       var body = await res.stream.bytesToString();
       setState(() {
         pneumoniaRisk = jsonDecode(body)['prediction'];
       });
-    } catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
@@ -39,77 +39,82 @@ class _PneumoniaInputState extends State<PneumoniaInput> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-            title: Text("Pneumonia Test Results")
-        ),
+        appBar: AppBar(title: Text("Pneumonia Test Results")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              FlatButton(
-                onPressed: () {
-                  uploadImage();
-                },
-                child: Container(
-                  width: 230,
-                  height: 45,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Upload XRay Image",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(26.0),
-                    color: const Color(0xff3f9bfc),
-                    border:
-                    Border.all(width: 1.0, color: const Color(0xff3f9bfc)),
-                  ),
-                ),
-              ),
-              ListTile(
-                  title: Text("Your Pneumonia Risk",
-                      style: TextStyle(
-                        fontSize: 17.5,
-                        fontWeight: FontWeight.w500,
-                      )
-                  ),
-                  subtitle: pneumoniaRisk > 80 && pneumoniaRisk <= 100 ? Text("High Risk",
-                      style: TextStyle(
-                          fontSize: 22.5,
+              // FlatButton(
+              //   onPressed: () {
+              //     uploadImage();
+              //   },
+              //   child: Container(
+              //     width: 230,
+              //     height: 45,
+              //     child: Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Text(
+              //         "Upload XRay Image",
+              //         style: TextStyle(
+              //             color: Colors.white,
+              //             fontSize: 22.5,
+              //             fontWeight: FontWeight.w500
+              //         ),
+              //         textAlign: TextAlign.center,
+              //       ),
+              //     ),
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(26.0),
+              //       color: const Color(0xff3f9bfc),
+              //       border:
+              //       Border.all(width: 1.0, color: const Color(0xff3f9bfc)),
+              //     ),
+              //   ),
+              // ),
+              Card(
+                child: ListTile(
+                    leading: FaIcon(FontAwesomeIcons.fileInvoice),
+                    title: Text("Your Pneumonia Risk",
+                        style: TextStyle(
+                          fontSize: 17.5,
                           fontWeight: FontWeight.w500,
-                          color: Colors.red
-                      )
-                  ) : pneumoniaRisk > 40 && pneumoniaRisk < 80 ? Text("Medium Risk",
-                      style: TextStyle(
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.yellow
-                      )
-                  ) : pneumoniaRisk < 40 && pneumoniaRisk > 20 ? Text("Low Risk",
-                      style: TextStyle(
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.green
-                      )
-                  ) : Text("Not Detected",
-                      style: TextStyle(
-                          fontSize: 22.5,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey
-                      )
-                  )
+                        )),
+                    subtitle: pneumoniaRisk > 80 && pneumoniaRisk <= 100
+                        ? Text("High Risk ($pneumoniaRisk %)",
+                            style: TextStyle(
+                                fontSize: 22.5,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.red))
+                        : pneumoniaRisk > 40 && pneumoniaRisk < 80
+                            ? Text("Medium Risk ($pneumoniaRisk %)",
+                                style: TextStyle(
+                                    fontSize: 22.5,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.yellow))
+                            : pneumoniaRisk < 40 && pneumoniaRisk > 20
+                                ? Text("Low Risk ($pneumoniaRisk %)",
+                                    style: TextStyle(
+                                        fontSize: 22.5,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.green))
+                                : Text("Not Detected",
+                                    style: TextStyle(
+                                        fontSize: 22.5,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.grey))),
               )
             ],
           ),
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          icon: FaIcon(FontAwesomeIcons.fileUpload),
+          onPressed: () {
+            uploadImage();
+          },
+          label: Text("Upload Chest X-Ray"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
   }
